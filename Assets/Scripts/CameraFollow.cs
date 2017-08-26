0d3af54f77048f23882725d40cea2d7c567b3db1
Sintaichi <shawnllacc@gmail.com>
2017-08-25 22:50:40 -0700

    started project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public static CameraFollow instance;
+
+	private Hero hero;
+
+	public void AttachToHero (Hero h) {
+		hero = h;
+	}
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (this);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (hero != null) {
+			transform.position = new Vector3 (hero.transform.position.x, hero.transform.position.y, transform.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+	private int damage;
+	private float speed;
+
+	void Awake() {
+		speed = 5;
+	}
+
+	void Start() {
+		CameraFollow.instance.AttachToHero (this);
+	}
+
+	void Update() {
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		transform.Translate (new Vector3 (x, y, 0) * Time.deltaTime * speed);
+	}
+
+}
