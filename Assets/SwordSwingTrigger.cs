46e1dee5033eb1bef84a006bc4cfe21d7e9dd9e3
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-08-26 14:55:48 -0700

    collider thing

--- Assets/SwordSwingTrigger.cs
+++ Assets/SwordSwingTrigger.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class SwordSwingTrigger : MonoBehaviour {
 
-
+	void Start() {
+		GetComponent<Collider2D> ().enabled = false;
+	}
 
 	public void SwordSwingEnd() {
 		Debug.Log ("Sword swung!");
+		GetComponent<Collider2D> ().enabled = false;
 	}
 
 	public void SwordSwingSwoosh() {
+
+		GetComponent<Collider2D> ().enabled = true;
 		Debug.Log ("should collide!");
 	}
 }
