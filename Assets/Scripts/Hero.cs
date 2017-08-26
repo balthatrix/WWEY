c9c42790719f71d51231a4ff314712e9868f957d
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-08-26 01:13:43 -0700

    left-click sword swign

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -7,15 +7,15 @@ public class Hero : MonoBehaviour {
 	// Editor-Visible Fields
 	public static Hero currentHero;
 	public Transform waist;
+	public Transform shoulders;
+
+
 
 	[SerializeField]
 	private int damage;
 	[SerializeField]
 	private float speed;
 
-	// Mono-Behavior Methods
-	void Awake() {
-	}
 
 	void Start() {
 		CameraFollow.instance.AttachToHero (this);
@@ -25,14 +25,43 @@ public class Hero : MonoBehaviour {
 		float x = Input.GetAxis ("Horizontal");
 		float y = Input.GetAxis ("Vertical");
 
-		transform.Translate (new Vector3 (x, y, 0) * Time.deltaTime * speed);
-
-		waist.rotation = Quaternion.Euler(0, 0, Util.ZDegFromDirection(x, y));
 
 
+
+		Vector3 intendedDirection = new Vector3 (x, y, 0f);
+		if (intendedDirection.sqrMagnitude > 1f) {
+			intendedDirection = intendedDirection.normalized;
+		}
+
+		//should be a value between 0, 1
+		FeetAnimator ().speed = intendedDirection.magnitude;
+
+
+		transform.Translate (intendedDirection * Time.deltaTime * speed);
+
+		if(Mathf.Abs(x) > 0f || Mathf.Abs(y) > 0f)
+			waist.rotation = Quaternion.Euler(0, 0, Util.ZDegFromDirection(x, y));
+
+		shoulders.rotation = Quaternion.Euler (0, 0, -Util.ZDegFromDirection (DirectionsToMouseInWorld()));
+
+		if (Input.GetMouseButtonDown (0)) { //left click
+			SwordAnimator().SetTrigger("Swing");
+		}
+	}
+
+	Vector2 DirectionsToMouseInWorld() {
+		return Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
 	}
 
 
+
+	Animator FeetAnimator() {
+		return waist.GetComponentInChildren<Animator> ();
+	}
+
+	Animator SwordAnimator() {
+		return shoulders.GetComponentInChildren<Animator> ();
+	}
 
 
 
@@ -44,6 +73,6 @@ public class Util {
 	}
 
 	public static float ZDegFromDirection(float x, float y) {
-		return Mathf.Atan2(x, y) * Mathf.Rad2Deg - 90;
+		return Mathf.Atan2(x, y) * Mathf.Rad2Deg;
 	}
 }
---
+++ Assets/SwordSwingTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwingTrigger : MonoBehaviour {
+
+
+
+	public void SwordSwingEnd() {
+		Debug.Log ("Sword swung!");
+	}
+
+	public void SwordSwingSwoosh() {
+		Debug.Log ("should collide!");
+	}
+}
