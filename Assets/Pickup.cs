99ff0e79d23eb78ba4824e53f34f3dbbb29f9169
Sintaichi <shawnllacc@gmail.com>
2017-08-27 17:08:07 -0700

    audio

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour {
 
+	public SoundSettingRandomizer pickUpSound;
 
 	public enum PickupType {
 		SWORD,
@@ -13,14 +14,21 @@ public class Pickup : MonoBehaviour {
 		ANNKE
 	}
 
+	void PlayPickupSound() {
+		pickUpSound.RandomizePlaySound ();
+	}
 
 	public PickupType myType;
 	void OnTriggerEnter2D(Collider2D other) {
 		PickupReceiver h = other.GetComponent<PickupReceiver> ();
 		if (h != null) {
 			h.DoPickup (this);
-
-			Destroy (gameObject);
+			if (pickUpSound != null) {
+				PlayPickupSound ();
+			}
+			gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+			gameObject.GetComponent<Collider2D> ().enabled = false;
+			Destroy (gameObject, 2f);
 		}
 	}
 
